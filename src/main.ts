581e7f036825e94ba871e259376c47249eacd255
giorgiogalassi <ged.galassi@gmail.com>
2024-03-02 16:37:22 +0100

    feat: add Inversity to achieve IoC

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import './style.css';
 import { Counter } from './counter.ts';
 import { ElementHandler } from './element.ts';
+import { Container } from 'inversify';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <div class="card">
@@ -8,7 +9,19 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     </div>
 `;
 
+// DI without IoC
 const elementHandler = new ElementHandler('#counter');
 const counter = new Counter(elementHandler);
 
 counter.setUpCounter();
+
+// DI with IoC using inversify
+const container = new Container();
+
+container.bind(Counter).toSelf();
+container.bind(ElementHandler).toSelf();
+
+const elementHandlerIoC = container.get(ElementHandler);
+const counterIoC = container.get(Counter);
+
+counterIoC.setUpCounter();
